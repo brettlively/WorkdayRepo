224184b20c273e9b2465acd061199e729bcf629b
bLively <brett.j.lively@gmail.com>
2023-04-23 15:27:19 -0600

    added UI and initial code in main activity

--- app/src/main/java/com/example/workdayapp/MainActivity.kt
+++ app/src/main/java/com/example/workdayapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.workdayapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var searchBar: SearchView
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //initialize
+        searchBar = findViewById(R.id.search_view)
+        recyclerView = findViewById(R.id.recycler_view)
+
+        //search
+        searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
+        })
+
+
+
+    }
+
+    private fun filterList(query: String?) {
+        //add search functionality
     }
 }
+
